11054896108e7ccbe93e30d1803119f1f9a996fc
Patryk Czeszejko <s21269@pjwstk.edu.pl>
2024-03-24 22:03:59 +0100

    Dodano metode showHighest

--- Program/Program/Program.cs
+++ Program/Program/Program.cs
@@ -2,7 +2,7 @@
     public static void Main(string[] args) {
         Console.WriteLine("Modyfikacja 3");
         int[] tab1 = { 1, 2, 3, 4, 5, 6 };
-        Console.WriteLine(showAverage(tab1));
+        Console.WriteLine(showHighest(tab1));
     }
 
     public static float showAverage(int[] a) {
@@ -15,5 +15,14 @@
             value += x;
         }
         return average = value/count;
+    }
+
+    public static int showHighest(int[] a) { 
+        int value = 0;
+        foreach (int x in a) {
+            if (x > value)
+                value = x;
+        }
+        return value;
     }
 }
