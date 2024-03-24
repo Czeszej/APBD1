4464b0f9f2c7ffb31dcae853c605fcbf4539c30e
Patryk Czeszejko <s21269@pjwstk.edu.pl>
2024-03-24 21:55:01 +0100

    Dodano metode showAverage

--- Program/Program/Program.cs
+++ Program/Program/Program.cs
@@ -1,5 +1,19 @@
 ﻿public class Program { 
     public static void Main(string[] args) {
         Console.WriteLine("Modyfikacja 3");
+        int[] tab1 = { 1, 2, 3, 4, 5, 6 };
+        Console.WriteLine(showAverage(tab1));
+    }
+
+    public static float showAverage(int[] a) {
+        float value = 0;
+        float count = 0;
+        float average = 0;
+
+        foreach (int x in a) { 
+            count ++;
+            value += x;
+        }
+        return average = value/count;
     }
 }
